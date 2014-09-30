45c1e77bb35575f83fef28f78f3e0f24d57d6af0
codup <codup@mail.ru>
2014-09-30 21:27:50 +0400

    move from bazaar

---
+++ mro_pm/wizard/replan.py
@@ -0,0 +1,33 @@
+﻿# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2013 CodUP (<http://codup.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, osv
+
+class mro_pm_replan(osv.osv_memory):
+    _name = 'mro.pm.replan'
+    _description = 'Replan PM'
+    
+    def replan_pm(self, cr, uid, ids, context=None):
+        self.pool.get('mro.order').replan_pm(cr, uid, context=context)
+        return {'type': 'ir.actions.act_window_close',}
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
