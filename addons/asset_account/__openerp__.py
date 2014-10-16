0908189e2b9d01147f4353982fe1184c8a40baaf
wowas18 <wowas18@nm.ru>
2014-10-16 09:41:19 +0400

    change version

--- addons/asset_account/__openerp__.py
+++ addons/asset_account/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Assets & Finance',
-    'version': '1.0',
+    'version': '1.1',
     'summary': 'Integrate Asset and Accounting',
     'description': """
 Integrate financial and maintenance asset management.
