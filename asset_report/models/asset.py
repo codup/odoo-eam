642783dfd2f705dc6340f45e9a20698dc39344cf
wowas18 <wowas18@nm.ru>
2015-11-13 13:31:59 +0400

    initial version

---
+++ asset_report/models/asset.py
@@ -0,0 +1,57 @@
+﻿# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 CodUP (<http://codup.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import api, models, fields
+
+
+WORK_TIME = [
+    ('0', 'Down Time'),
+    ('1', 'Up Time')
+]
+
+class asset_state_history(models.Model):
+    _name = 'asset.state.history'
+    _description = 'History of Asset State'
+    
+    asset_id = fields.Many2one('asset.asset', 'Asset')
+    maintenance_state_id = fields.Many2one('asset.state', 'State', domain=[('team','=','3')])
+
+
+class asset_state(models.Model):
+    _inherit = 'asset.state'
+
+    work_time = fields.Selection(WORK_TIME, 'Work')
+
+
+class asset_asset(models.Model):
+    _inherit = 'asset.asset'
+
+    @api.multi
+    def write(self, vals):
+        if vals.get('maintenance_state_id', False):
+            for asset in self:
+                self.env['asset.state.history'].create({
+                    'maintenance_state_id': vals.get('maintenance_state_id', False),
+                    'asset_id': asset.id
+                    })
+        return super(asset_asset, self).write(vals)
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ asset_report/report/asset_state_log.py
@@ -0,0 +1,49 @@
+﻿# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 CodUP (<http://codup.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import tools
+from openerp import models, fields, api
+from openerp.addons.asset_report.models.asset import WORK_TIME
+
+
+class AssetStateLog(models.Model):
+    _name = "asset.state.log"
+    _description = "Asset State Logs"
+    _auto = False
+    _rec_name = 'date'
+
+    asset_id = fields.Many2one('asset.asset', 'Asset', readonly=True)
+    maintenance_state_id = fields.Many2one('asset.state', 'State', readonly=True)
+    work_time = fields.Selection(WORK_TIME, 'Work', related='maintenance_state_id.work_time', readonly=True)
+    duration = fields.Float('Duration(H)', readonly=True)
+    date = fields.Datetime('Date', readonly=True)
+
+    _order = 'asset_id, date desc'
+
+    def init(self, cr):
+        tools.drop_view_if_exists(cr, self._table)
+        cr.execute("""CREATE or REPLACE VIEW %s as (
+            SELECT id, asset_id, maintenance_state_id, create_date date,
+            (EXTRACT(EPOCH FROM LEAD(create_date,1,now() at time zone 'utc') OVER (PARTITION BY asset_id ORDER BY create_date)) - EXTRACT(EPOCH FROM create_date))/3600 duration
+            FROM asset_state_history
+        )""" % (self._table))
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
