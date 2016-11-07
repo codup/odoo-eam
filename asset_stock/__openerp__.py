599d456e93e7469531a74cf997a6fb08c620b430
codup <wowas18@nm.ru>
2016-11-07 18:41:30 +0400

    change version

--- asset_stock/__openerp__.py
+++ asset_stock/__openerp__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'Assets & Warehouse',
-    'version': '1.2',
+    'version': '1.3',
     'summary': 'Integrate Asset and Warehouse',
     'description': """
 Integrate Maintenance and Warehouse.
