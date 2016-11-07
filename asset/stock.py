d27e289e836bc8f075754ff74c30ee9ba2705523
codup <wowas18@nm.ru>
2016-11-07 17:45:04 +0400

    port to Odoo 10.0

--- asset/stock.py
+++ asset/stock.py
@@ -1,52 +1,38 @@
 ﻿# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2013 CodUP (<http://codup.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    Odoo
+#    Copyright (C) 2013-2016 CodUP (<http://codup.com>).
 #
 ##############################################################################
 
-from openerp.osv import fields, osv
+from odoo import api, fields, models
 
-class stock_location(osv.osv):
+class stock_location(models.Model):
     _inherit = "stock.location"
 
     TYPE_SELECTION = [
-        ('supplier', 'Supplier Location'),
+        ('supplier', 'Vendor Location'),
         ('view', 'View'),
         ('internal', 'Internal Location'),
         ('customer', 'Customer Location'),
-        ('inventory', 'Inventory'),
+        ('inventory', 'Inventory Loss'),
         ('procurement', 'Procurement'),
         ('production', 'Production'),
-        ('transit', 'Transit Location for Inter-Companies Transfers'),
+        ('transit', 'Transit Location'),
         ('asset', 'Asset Location')
     ]
-    
-    _columns = {
-        'usage': fields.selection(TYPE_SELECTION, 'Location Type', required=True,
-                 help="""* Supplier Location: Virtual location representing the source location for products coming from your suppliers
-                       \n* View: Virtual location used to create a hierarchical structures for your warehouse, aggregating its child locations ; can't directly contain products
-                       \n* Internal Location: Physical locations inside your own warehouses,
-                       \n* Customer Location: Virtual location representing the destination location for products sent to your customers
-                       \n* Inventory: Virtual location serving as counterpart for inventory operations used to correct stock levels (Physical inventories)
-                       \n* Procurement: Virtual location serving as temporary counterpart for procurement operations when the source (supplier or production) is not known yet. This location should be empty when the procurement scheduler has finished running.
-                       \n* Production: Virtual counterpart location for production operations: this location consumes the raw material and produces finished products
-                       \n* Asset Location: Physical locations representing the destination location for parts installed on the asset (Maintenance and Repair)
-                      """, select = True),
-    }
+
+    usage = fields.Selection(TYPE_SELECTION, string='Location Type',
+        default='internal', index=True, required=True,
+        help="* Vendor Location: Virtual location representing the source location for products coming from your vendors"
+             "\n* View: Virtual location used to create a hierarchical structures for your warehouse, aggregating its child locations ; can't directly contain products"
+             "\n* Internal Location: Physical locations inside your own warehouses,"
+             "\n* Customer Location: Virtual location representing the destination location for products sent to your customers"
+             "\n* Inventory Loss: Virtual location serving as counterpart for inventory operations used to correct stock levels (Physical inventories)"
+             "\n* Procurement: Virtual location serving as temporary counterpart for procurement operations when the source (vendor or production) is not known yet. This location should be empty when the procurement scheduler has finished running."
+             "\n* Production: Virtual counterpart location for production operations: this location consumes the raw material and produces finished products"
+             "\n* Transit Location: Counterpart location that should be used in inter-companies or inter-warehouses operations"
+             "\n* Asset Location: Physical locations representing the destination location for parts installed on the asset (Maintenance and Repair)")
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
