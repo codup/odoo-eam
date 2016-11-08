0893eeec946298dac83eb63c26f39c9e52a97e39
codup <wowas18@nm.ru>
2016-11-08 14:21:16 +0400

    change version

--- asset_mrp/__openerp__.py
+++ asset_mrp/__openerp__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'Assets & Manufacturing',
-    'version': '1.2',
+    'version': '1.3',
     'summary': 'Integrate Asset and Manufacturing',
     'description': """
 Integrate manufacturing and maintenance asset management.
