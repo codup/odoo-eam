b510624c292219ce0d6f916c292063bff8af91e0
codup <wowas18@nm.ru>
2016-11-08 14:18:52 +0400

    port to Odoo 10.0

--- asset_mrp/asset.py
+++ asset_mrp/asset.py
@@ -1,32 +1,18 @@
 ﻿# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2014 CodUP (<http://codup.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    Odoo
+#    Copyright (C) 2014-2016 CodUP (<http://codup.com>).
 #
 ##############################################################################
 
-from openerp.osv import fields, osv
-from openerp.addons.asset.asset import STATE_COLOR_SELECTION
+from odoo import api, fields, models
+from odoo.addons.asset.asset import STATE_COLOR_SELECTION
 
-class asset_asset(osv.osv):
+
+class asset_asset(models.Model):
     _inherit = 'asset.asset'
 
-    _columns = {
-        'manufacture_state_color': fields.related('manufacture_state_id', 'state_color', type="selection", selection=STATE_COLOR_SELECTION, string="Color", readonly=True),
-    }
+    manufacture_state_color = fields.Selection(related='manufacture_state_id.state_color', selection=STATE_COLOR_SELECTION, string="Color", readonly=True)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- asset_mrp/mrp.py
+++ asset_mrp/mrp.py
@@ -1,48 +1,32 @@
 ﻿# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2013-2014 CodUP (<http://codup.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    Odoo
+#    Copyright (C) 2013-2016 CodUP (<http://codup.com>).
 #
 ##############################################################################
 
-from openerp.osv import fields, osv
+from odoo import api, fields, models
 
-class mrp_workcenter(osv.osv):
+
+class mrp_workcenter(models.Model):
     _inherit = 'mrp.workcenter'
-    _columns = {
-        'asset_ids': fields.many2many('asset.asset', string='Asset'),
-    }
+
+    asset_ids = fields.Many2many('asset.asset', string='Asset')
 
 
-class mrp_bom(osv.osv):
+class mrp_bom(models.Model):
     _inherit = 'mrp.bom'
 
-    def _get_assets(self, cr, uid, ids, name, arg, context=None):
-        res = {}
-        for bom in self.browse(cr, uid, ids, context=context):
+    @api.depends('routing_id')
+    def _get_assets(self):
+        for bom in self:
             line_ids = []
             if bom.routing_id:
-                for work_center in bom.routing_id.workcenter_lines:
+                for work_center in bom.routing_id.operation_ids:
                     line_ids += [asset.id for asset in work_center.workcenter_id.asset_ids]
-            res[bom.id] = line_ids
-        return res
+            bom.asset_ids = line_ids
 
-    _columns = {
-        'asset_ids': fields.function(_get_assets, relation="asset.asset", method=True, type="one2many"),
-    }
+    asset_ids = fields.One2many('asset.asset', compute='_get_assets')
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
