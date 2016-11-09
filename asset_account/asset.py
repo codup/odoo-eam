92ec10e1373f9cb4984539d011e9cdb40436423c
codup <wowas18@nm.ru>
2016-11-09 13:25:45 +0400

    port to Odoo 10.0

--- asset_account/asset.py
+++ asset_account/asset.py
@@ -1,35 +1,21 @@
 ﻿# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2013 CodUP (<http://codup.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    Odoo
+#    Copyright (C) 2013-2016 CodUP (<http://codup.com>).
 #
 ##############################################################################
 
-from openerp.osv import fields, osv
+from odoo import api, fields, models
 
-class account_asset(osv.osv):
+
+class AccountAssetAsset(models.Model):
     _inherit = 'account.asset.asset'
 
-    _columns = {
-        'asset_id': fields.many2one('asset.asset', 'Asset', required=True),
-    }
+    asset_id = fields.Many2one('asset.asset', 'Asset', required=True)
 
-    def onchange_asset(self, cr, uid, ids, asset, context=None):
-        asset = self.pool.get('asset.asset').browse(cr, uid, asset, context=context)
-        return {'value': {'name': asset.name}}
+    @api.onchange('asset_id')
+    def onchange_asset(self):
+        self.name = self.asset_id.name
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
