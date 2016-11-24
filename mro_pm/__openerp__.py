f683522ad15cb5e4e1da99f4f9e614e7a8ee8d9f
codup <wowas18@nm.ru>
2016-11-24 19:06:51 +0400

    change version

--- mro_pm/__openerp__.py
+++ mro_pm/__openerp__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'MRO PM',
-    'version': '1.3',
+    'version': '1.4',
     'summary': 'Asset Proactive Maintenance',
     'description': """
 Manage Proactive Maintenance process in OpenERP
