8b16f33805ff993b7250d6dfd9204fd9e739f8ad
codup <wowas18@nm.ru>
2017-03-10 11:39:38 +0400

    fix _check_category()

--- mro/product_template.py
+++ mro/product_template.py
@@ -13,6 +13,8 @@ class ProductTemplate(models.Model):
     _name = "product.template"
     _inherit = "product.template"
 
+    @api.multi
+    @api.depends('categ_id')
     def _check_category(self):
         parts_category = self.env.ref('mro.product_category_mro', raise_if_not_found=False)
         for product in self:
