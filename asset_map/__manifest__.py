729c8c00fed3db034e3f0a99d0f5355f7df2906d
wowas18 <wowas18@nm.ru>
2017-03-25 19:08:30 +0400

    initial version

---
+++ asset_map/__manifest__.py
@@ -0,0 +1,30 @@
+﻿# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo
+#    Copyright (C) 2017 CodUP (<http://codup.com>).
+#
+##############################################################################
+
+{
+    'name': 'Asset Map',
+    'version': '0.1',
+    'category': 'Industries',
+    'summary': 'Show asset position',
+    'description': """
+Show Asset on Map
+==========================
+Support following feature:
+    * Edit asset position
+    """,
+    'author': 'CodUP',
+    'license': 'AGPL-3',
+    'website': 'http://codup.com',
+    'sequence': 0,
+    'depends': ['asset','web_map'],
+    'data': [
+        'views/asset_view.xml',
+    ],
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ asset_map/models/asset.py
@@ -0,0 +1,17 @@
+﻿# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo
+#    Copyright (C) 2017 CodUP (<http://codup.com>).
+#
+##############################################################################
+
+from odoo import api, fields, models, _
+
+
+class asset_asset(models.Model):
+    _inherit = 'asset.asset'
+
+    position = fields.Char('Map')
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
