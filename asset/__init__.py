f76b90e2f99e8efbaf127ee81274c2b915e7625e
Paolo Bellagente <paolo.bellagente@gmail.com>
2018-02-12 15:07:14 +0100

    cannot find module asset fixed

--- asset/__init__.py
+++ asset/__init__.py
@@ -19,7 +19,7 @@
 #
 ##############################################################################
 
-import asset
-import stock
+from . import asset
+from . import stock
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
