f1c5dfb85c6947d9676446fd4a9bd6f77c9d8c91
wowas18 <wowas18@nm.ru>
2018-06-15 18:55:13 +0400

    remove workflow

--- mro/stock.py
+++ mro/stock.py
@@ -14,11 +14,11 @@ class StockMove(models.Model):
     @api.model
     def write(self, vals):
         res = super(StockMove, self).write(vals)
-        from odoo import workflow
-        if vals.get('state') == 'assigned':
-            mro_obj = self.env['mro.order']
-            order_ids = mro_obj.search([('procurement_group_id', 'in', [x.group_id.id for x in self])])
-            for order_id in order_ids:
-                if order_id.test_ready():
-                    workflow.trg_validate(self.env.user.id, 'mro.order', order_id.id, 'parts_ready', self.env.cr)
+        # from odoo import workflow
+        # if vals.get('state') == 'assigned':
+            # mro_obj = self.env['mro.order']
+            # order_ids = mro_obj.search([('procurement_group_id', 'in', [x.group_id.id for x in self])])
+            # for order_id in order_ids:
+                # if order_id.test_ready():
+                    # workflow.trg_validate(self.env.user.id, 'mro.order', order_id.id, 'parts_ready', self.env.cr)
         return res
