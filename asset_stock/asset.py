a66ab0d454b9cc44f7ee71832bbef862a416bc95
wowas18 <wowas18@nm.ru>
2018-06-15 16:53:21 +0400

    fix encoding

--- asset_stock/asset.py
+++ asset_stock/asset.py
@@ -1,4 +1,4 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Odoo
@@ -14,5 +14,3 @@ class asset_asset(models.Model):
     _inherit = 'asset.asset'
 
     warehouse_state_color = fields.Selection(related='warehouse_state_id.state_color', selection=STATE_COLOR_SELECTION, string="Color", readonly=True)
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
