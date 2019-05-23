3464ffbd528e1477f5077cacd56209648d03929f
wowas18 <wowas18@nm.ru>
2019-05-23 14:21:08 +0400

    fix reject

--- mro/wizard/reject.py
+++ mro/wizard/reject.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo
-#    Copyright (C) 2013-2018 CodUP (<http://codup.com>).
+#    Copyright (C) 2013-2019 CodUP (<http://codup.com>).
 #
 ##############################################################################
 
@@ -20,6 +20,5 @@ class mro_request_reject(models.TransientModel):
         if active_id:
             request = self.env['mro.request'].browse(self._context.get('active_id'))
             request.write({'reject_reason':self.reject_reason})
-            wf_service = netsvc.LocalService("workflow")
-            wf_service.trg_validate(self.env.user.id, 'mro.request', active_id, 'button_reject', self.env.cr)
+            request.action_reject()
         return {'type': 'ir.actions.act_window_close',}
